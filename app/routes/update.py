3e997c37ac9dcb3eef68b0d7c1a87d95b5941467
Jacobchestnut16 <88856888+Jacobchestnut16@users.noreply.github.com>
2025-07-18 21:51:05 -0400

    beta v2.0.0 b2

--- app/routes/update.py
+++ app/routes/update.py
@@ -198,7 +198,8 @@ def update():
     update = check_for_updates()
     if update[0] == "Update required":
         return render_template('update.html', updateTxt=update[0], updateVersion=update[1], current=current,
-                               system_theme=config_background())
+                               system_theme=config_background(), update_desc=fetch_remote_file(BASE_URL+'update_dec'))
     else:
-        return render_template('update.html', updateTxt=update[0], current=current, system_theme=config_background())
+        return render_template('update.html', updateTxt=update[0], current=current,
+                               system_theme=config_background(), update_desc=fetch_remote_file(BASE_URL+'update_dec'))
 
